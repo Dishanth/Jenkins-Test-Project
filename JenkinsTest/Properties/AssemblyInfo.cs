591ae232f5517c231a0126fd1cb2e7e382bdea6f
Dishanth Nagarajaiah <ndishanthn@gmail.com>
2019-10-17 13:42:43 -0400

    Added build code

---
+++ JenkinsTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("JenkinsTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("JenkinsTest")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("8d634d9a-2e9c-4ba5-a5e1-f8d4e1873154")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ JenkinsTest/UnitTest1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using BuildJenkins;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace JenkinsTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+          Assert.AreEqual("First CICD", Program.CreateMessage(),"Failed to match string");
+        }
+    }
+}
