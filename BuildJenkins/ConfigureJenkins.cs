c7e9206c37875ccf724249a95c7c66d1c15cf7f4
Dishanth Nagarajaiah <ndishanthn@gmail.com>
2019-11-12 15:34:44 -0500

    Changed to class lib

---
+++ BuildJenkins/ConfigureJenkins.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BuildJenkins
+{
+    class ConfigureJenkins
+    {
+
+        //Step1 : Global tool configuration
+        //Use chocolatey to download tools , this just like nuget package but for windows ,
+        //This also sets the environment variable s during the install
+        //go to https://chocolatey.org/ and install chocolatey (its one time installation)
+
+
+
+    }
+}
